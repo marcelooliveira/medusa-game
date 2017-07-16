2efa433cab0b19ad9606554e3317ccef21046f33
marcelooliveira <mclricardo@gmail.com>
2017-07-16 13:09:29 -0300

    .
    .

--- Medusa/app.ts
+++ Medusa/app.ts
@@ -3,6 +3,9 @@
 
 class MedusaGame {
     game: Phaser.Game;
+    map: Phaser.Tilemap;
+    bmd: Phaser.Image;
+    layer: Phaser.TilemapLayer;
     tileSprite: Phaser.TileSprite;
     player: Phaser.Sprite;
     bossSprite: Phaser.Sprite;
@@ -11,7 +14,7 @@ class MedusaGame {
     constructor() {
         this.game = new Phaser.Game(512, 512, Phaser.AUTO, 'content', {
             create: this.create, preload: this.preload,
-            update: this.update
+            update: this.update, readFile: this.readFile
             //, render: this.render
         });
     }
@@ -27,38 +30,66 @@ class MedusaGame {
 
         this.game.world.setBounds(0, 0, 512, 3776);
 
+        //  Creates a blank tilemap
+        this.map = this.game.add.tilemap();
+
+        //this.map.addTilesetImage('tiles', this.tileSprite);
+
+        this.layer = this.map.create('level1', 16, 118, 32, 32);
+
+
+
+        var lines = this.readFile("/assets/maps/Map01.txt").split('\n');
+        for (var y = 0; y < lines.length; y++)
+        {
+            var line = lines[y];
+            for (var x = 0; x < line.length; x++) {
+                var char = line[x];
+                if (char == '.') {
+                    this.map.putTile(1, x, y, this.layer);
+                }
+            }
+        }
+        this.map.setCollisionByExclusion([0]);
+
+
+
+        //  Add a Tileset image to the map
+        //this.map.addTilesetImage('tiles', bmd);
+
         this.game.physics.startSystem(Phaser.Physics.P2JS);
         this.cursors = this.game.input.keyboard.createCursorKeys();
-        this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+        //this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+        this.player = this.game.add.sprite(96, 96, 'player');
 
         this.game.physics.p2.enable(this.player);
-
+        
         this.player.body.fixedRotation = true;
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
         this.game.debug.text('Press down arrow keys to move the tileSprite', 20, 20);
 
-        this.player.anchor.setTo(0.5, 0.5);
-        //player.scale.setTo(2, 2);
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 
         this.player.animations.add('run');
         this.player.animations.play('run', 3, true);
+        this.player.body.collideWorldBounds = true;
 
         this.bossSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'boss');
         this.bossSprite.anchor.setTo(0.75, 3.5);
-        //player.scale.setTo(2, 2);
 
         this.bossSprite.animations.add('run');
         this.bossSprite.animations.play('run', 2, true);
 
     }
-
+            
     update() {
-        // Update input state
+       
         this.player.body.setZeroVelocity();
         this.game.input.update();
+        //this.game.physics.arcade.collide(this.player, this.layer);
 
         if (this.cursors.up.isDown) {
             this.player.body.moveUp(180)
@@ -77,6 +108,15 @@ class MedusaGame {
 
     render() {
         this.game.debug.cameraInfo(this.game.camera, 32, 32);
+    }
+
+    readFile(file: string) : string {
+        var request = new XMLHttpRequest();
+        request.open("GET", file, false);
+        request.send(null);
+        var returnValue = request.responseText;
+
+        return returnValue;
     }
 }
 
