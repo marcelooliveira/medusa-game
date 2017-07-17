9c0fdaf3c61c4bb752d69ab6b85984a6b577b901
marcelooliveira <mclricardo@gmail.com>
2017-07-16 23:30:32 -0300

    the map collision is working!
    the map collision is working!

--- Medusa/app.ts
+++ Medusa/app.ts
@@ -4,18 +4,19 @@
 class MedusaGame {
     game: Phaser.Game;
     map: Phaser.Tilemap;
-    bmd: Phaser.Image;
+    bmd: Phaser.BitmapData;
     layer: Phaser.TilemapLayer;
     tileSprite: Phaser.TileSprite;
     player: Phaser.Sprite;
     bossSprite: Phaser.Sprite;
     cursors: Phaser.CursorKeys;
+    playerVelocity: number;
 
     constructor() {
         this.game = new Phaser.Game(512, 512, Phaser.AUTO, 'content', {
             create: this.create, preload: this.preload,
-            update: this.update, readFile: this.readFile
-            //, render: this.render
+            update: this.update, readFile: this.readFile,
+            playerVelocity: this.playerVelocity
         });
     }
 
@@ -27,17 +28,34 @@ class MedusaGame {
 
     create() {
         this.tileSprite = this.game.add.tileSprite(0, 0, 512, 3776, 'level');
-
         this.game.world.setBounds(0, 0, 512, 3776);
 
         //  Creates a blank tilemap
         this.map = this.game.add.tilemap();
 
-        //this.map.addTilesetImage('tiles', this.tileSprite);
+        //  This is our tileset - it's just a BitmapData filled with a selection of randomly colored tiles
+        //  but you could generate anything here
+        this.bmd = this.game.make.bitmapData(32 * 25, 32 * 2);
 
+        var colors = Phaser.Color.HSVColorWheel();
+
+        var i = 0;
+
+        for (var y = 0; y < 2; y++) {
+            for (var x = 0; x < 25; x++) {
+                //this.bmd.rect(x * 32, y * 32, 32, 32, colors[i].rgba);
+                i += 6;
+            }
+        }
+
+        //  Add a Tileset image to the map
+        this.map.addTilesetImage('tiles', this.bmd);
+
+        //  Creates a new blank layer and sets the map dimensions.
+        //  In this case the map is 40x30 tiles in size and the tiles are 32x32 pixels in size.
         this.layer = this.map.create('level1', 16, 118, 32, 32);
-        this.layer.resizeWorld();
 
+        //  Populate some tiles for our player to start on
 
         var lines = this.readFile("/assets/maps/Map01.txt").split('\n');
         for (var y = 0; y < lines.length; y++)
@@ -45,76 +63,50 @@ class MedusaGame {
             var line = lines[y];
             for (var x = 0; x < line.length; x++) {
                 var char = line[x];
-                if (char == '.') {
-                    //this.map.putTile(1, x, y, this.layer);
+                if (char == 'X') {
+                    this.map.putTile(1, x, y, this.layer);
                 }
             }
         }
 
-        this.map.putTile(1, 2, 10, this.layer);
-        this.map.putTile(1, 3, 10, this.layer);
-        this.map.putTile(1, 4, 10, this.layer);
         this.map.setCollisionByExclusion([0]);
 
-
-
-        //  Add a Tileset image to the map
-        //this.map.addTilesetImage('tiles', bmd);
-
-        //this.game.physics.startSystem(Phaser.Physics.P2JS);
-
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-        //this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
-        this.player = this.game.add.sprite(96, 96, 'player');
-        this.game.physics.arcade.enable(this.player);
-
-        //this.game.physics.p2.enable(this.player);
-        
-        //this.player.body.fixedRotation = true;
-
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-
-        this.game.debug.text('Press down arrow keys to move the tileSprite', 20, 20);
-
-        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
-        this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
-
+        this.player = this.game.add.sprite(this.game.world.centerX - 16, this.game.world.height - 64, 'player');
         this.player.animations.add('run');
         this.player.animations.play('run', 3, true);
-        this.player.body.collideWorldBounds = true;
-        this.player.body.setSize(20, 32, 5, 16);
+        this.playerVelocity = 150;
+        this.game.physics.arcade.enable(this.player);
 
-        this.bossSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'boss');
-        this.bossSprite.anchor.setTo(0.75, 3.5);
+        this.player.body.collideWorldBounds = true;
+        this.player.body.setSize(32, 32, 0, 0);
+        this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        this.bossSprite = this.game.add.sprite(this.game.world.centerX - 48, 64, 'boss');
 
         this.bossSprite.animations.add('run');
         this.bossSprite.animations.play('run', 2, true);
-
     }
             
     update() {
        
-        //this.player.body.setZeroVelocity();
-        //this.player.body.position = { x: 0, y: 0 };
         this.game.input.update();
         this.game.physics.arcade.collide(this.player, this.layer);
 
         this.player.body.velocity.set(0);
         if (this.cursors.up.isDown) {
-            //this.player.body.moveUp(180)
-            this.player.body.velocity.y = -100;
+            this.player.body.velocity.y = -this.playerVelocity;
         }
         else if (this.cursors.down.isDown) {
-            //this.player.body.moveDown(180);
-            this.player.body.velocity.y = 100;
+            this.player.body.velocity.y = this.playerVelocity;
         }
 
         if (this.cursors.left.isDown) {
-            this.player.body.velocity.x = -100;
+            this.player.body.velocity.x = -this.playerVelocity;
         }
         else if (this.cursors.right.isDown) {
-            //this.player.body.moveRight(180);
-            this.player.body.velocity.x = 100;
+            this.player.body.velocity.x = this.playerVelocity;
         }
     }
 
